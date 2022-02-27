e7da61a643f506e87e4f4d11e0671cc4d611de2e
4825764518 <100122841+4825764518@users.noreply.github.com>
2022-02-27 00:57:58 -0500

    stormveil: init

---
+++ hosts/stormveil/hardware-configuration.nix
@@ -0,0 +1,31 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "uas" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/56bc1b4a-6203-48f2-ac8f-5dd942518bd7";
+      fsType = "btrfs";
+      options = [ "ssd" "compress-force=zstd:1" "space_cache=v2" "discard=async" ];
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/934C-9FE2";
+      fsType = "vfat";
+    };
+
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/05a4d4bc-b839-47fe-8e75-b17202a5b43d"; }
+    ];
+
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  # high-resolution display
+  hardware.video.hidpi.enable = lib.mkDefault true;
+}
