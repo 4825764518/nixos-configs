41e0b244c8e52b73da96ce287d8193d5b83896e0
4825764518 <100122841+4825764518@users.noreply.github.com>
2022-02-27 18:44:02 -0500

    home-manager: init

--- flake.nix
+++ flake.nix
@@ -4,11 +4,12 @@
     nixos.url = "github:nixos/nixpkgs/nixos-unstable";
     nix-darwin.url = "github:lnl7/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
     sops-nix.url = "github:Mic92/sops-nix";
     # sops-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, nixos, nix-darwin, sops-nix }:
+  outputs = { self, nixpkgs, nixos, nix-darwin, home-manager, sops-nix }:
     let
       pkgsNonfree-linux-x64 = import nixos {
         system = "x86_64-linux";
@@ -27,14 +28,32 @@
         };
         stormveil = nixos.lib.nixosSystem {
           system = "x86_64-linux";
-          modules = [ ./hosts/stormveil/configuration.nix sops-nix.nixosModules.sops ];
+          modules = [
+            ./hosts/stormveil/configuration.nix
+            sops-nix.nixosModules.sops
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.verbose = true;
+              home-manager.users.kenzie = import ./home/home.nix;
+            }
+          ];
           pkgs = pkgsNonfree-linux-x64;
         };
       };
       darwinConfigurations = {
         interloper = nix-darwin.lib.darwinSystem {
           system = "aarch64-darwin";
-          modules = [ ./hosts/interloper/darwin-configuration.nix ];
+          modules = [
+            ./hosts/interloper/darwin-configuration.nix
+            home-manager.darwinModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.kenzie = import ./home/home.nix;
+            }
+          ];
           pkgs = pkgsNonfree-darwin-aarch64;
         };
       };
---
+++ home/home.nix
@@ -0,0 +1,6 @@
+{ pkgs, lib, config, ... }:
+
+{
+  config.programs.home-manager.enable = true;
+  config.programs.bash.enable = true;
+}
