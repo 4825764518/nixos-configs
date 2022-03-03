2012c0b2dc849185efcf6bb772bccd6420e7d333
4825764518 <100122841+4825764518@users.noreply.github.com>
2022-03-02 23:19:49 -0500

    linux-desktop: add various emulators

--- apps/linux-desktop/games.nix
+++ apps/linux-desktop/games.nix
@@ -1,5 +1,5 @@
 { config, pkgs, ... }: {
-  environment.systemPackages = with pkgs; [ polymc ];
+  environment.systemPackages = with pkgs; [ citra dolphin-emu-beta pcsx2 polymc rpcs3 yuzu-ea ];
 
   programs.steam.enable = true;
 }
