ea6e25ee9d1678f7e8b517c80735357869b0da22
4825764518 <100122841+4825764518@users.noreply.github.com>
2022-03-08 20:52:56 -0500

    linux-desktop: add lutris

--- apps/linux-desktop/games.nix
+++ apps/linux-desktop/games.nix
@@ -1,5 +1,13 @@
 { config, pkgs, ... }: {
-  environment.systemPackages = with pkgs; [ citra dolphin-emu-beta pcsx2 polymc rpcs3 yuzu-ea ];
+  environment.systemPackages = with pkgs; [
+    citra
+    dolphin-emu-beta
+    lutris
+    pcsx2
+    polymc
+    rpcs3
+    yuzu-ea
+  ];
 
   programs.steam.enable = true;
 }
