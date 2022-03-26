ba8c8dbba90555ae859fe92ba24ab19282ea39ff
4825764518 <100122841+4825764518@users.noreply.github.com>
2022-03-26 13:58:27 -0400

    linux-common: enable zswap, add kernel options for z3fold and zstd

--- hosts/linux-common.nix
+++ hosts/linux-common.nix
@@ -3,6 +3,17 @@
 {
   imports = [ ../apps/common.nix ../apps/nix.nix ];
 
+  boot.initrd.kernelModules = [ "zstd" "z3fold" ];
+  boot.kernelParams =
+    [ "zswap.enabled=1" "zswap.compressor=zstd" "zswap.zpool=z3fold" ];
+  boot.kernelPatches = [{
+    name = "zswap";
+    patch = null;
+    extraConfig = ''
+      Z3FOLD y
+      CRYPTO_ZSTD y
+    '';
+  }];
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
