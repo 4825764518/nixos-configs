0d36fdcac0ce36324fb572b9ae776559b6e2be38
4825764518 <100122841+4825764518@users.noreply.github.com>
2022-03-26 13:51:45 -0400

    home-manager: add zsh nix-zsh-completions and zsh-nix-shell plugins

--- home/modules/shell.nix
+++ home/modules/shell.nix
@@ -29,6 +29,29 @@ in {
         enable = true;
         theme = "strug";
       };
+
+      plugins = [
+        {
+          name = "nix-zsh-completions";
+          file = "nix-zsh-completions.plugin.zsh";
+          src = pkgs.fetchFromGitHub {
+            owner = "spwhitt";
+            repo = "nix-zsh-completions";
+            rev = "0.4.4";
+            sha256 = "1n9whlys95k4wc57cnz3n07p7zpkv796qkmn68a50ygkx6h3afqf";
+          };
+        }
+        {
+          name = "zsh-nix-shell";
+          file = "nix-shell.plugin.zsh";
+          src = pkgs.fetchFromGitHub {
+            owner = "chisui";
+            repo = "zsh-nix-shell";
+            rev = "v0.4.0";
+            sha256 = "037wz9fqmx0ngcwl9az55fgkipb745rymznxnssr3rx9irb6apzg";
+          };
+        }
+      ];
     };
   };
 }
