0db11d897f0a16551bcb65ff8f5070691c0dfebe
4825764518 <100122841+4825764518@users.noreply.github.com>
2022-04-04 13:26:30 -0400

    linux-common: enable fstrim

--- hosts/linux-common.nix
+++ hosts/linux-common.nix
@@ -17,6 +17,9 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  services.fstrim.enable = true;
+  services.fstrim.interval = "weekly";
+
   environment.systemPackages = with pkgs; [
     btrfs-progs
     compsize
