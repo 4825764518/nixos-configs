bacb11b6b044e4b71a08d57993e65ca8960e0fdc
4825764518 <100122841+4825764518@users.noreply.github.com>
2022-04-06 03:44:31 -0400

    home-manager: update gtk config for gtk4

--- home/modules/gtk.nix
+++ home/modules/gtk.nix
@@ -10,21 +10,39 @@ in (lib.mkIf isLinux {
       package = pkgs.fira-mono;
     };
     iconTheme = {
-      name = "Arc";
-      package = pkgs.arc-icon-theme;
+      name = "WhiteSur-dark";
+      package = pkgs.whitesur-icon-theme;
     };
     theme = {
-      name = "Arc-Dark";
-      package = pkgs.arc-theme;
+      name = "WhiteSur-dark-solid-pink";
+      package = pkgs.whitesur-gtk-theme.overrideAttrs (oldAttrs: {
+        installPhase = ''
+          runHook preInstall
+          mkdir -p $out/share/themes
+          ./install.sh --dest $out/share/themes -i simple --alt all --theme all 
+          runHook postInstall
+        '';
+      });
     };
     gtk2.extraConfig = ''
-      gtk-icon-name="Arc"
+      gtk-icon-theme-name="WhiteSur-dark"
+      gtk-theme-name="WhiteSur-dark-solid-pink"
+      gtk-font-name="Fira Mono 10"
     '';
     gtk3.extraConfig = {
       "gtk-application-prefer-dark-theme" = 1;
+      "gtk-font-name" = "Fira Mono 10";
+      "gtk-icon-theme-name" = "WhiteSur-dark";
+      "gtk-theme-name" = "WhiteSur-dark-solid-pink";
+      "gtk-toolbar-style" = "GTK_TOOLBAR_BOTH_HORIZ";
+    };
+    gtk4.extraConfig = {
+      "gtk-application-prefer-dark-theme" = 1;
       "gtk-decoration-layout" = "menu:minimize,maximize,close";
-      "gtk-icon-theme-name" = "Arc";
-      "gtk-theme-name" = "Arc-Dark";
+      "gtk-font-name" = "Fira Mono 10";
+      "gtk-hint-font-metrics" = 1;
+      "gtk-icon-theme-name" = "WhiteSur-dark";
+      "gtk-theme-name" = "WhiteSur-dark-solid-pink";
       "gtk-toolbar-style" = "GTK_TOOLBAR_BOTH_HORIZ";
     };
   };
