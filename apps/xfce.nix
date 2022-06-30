33d96949949782d893d753925c7cacdb1494f0c0
4825764518 <100122841+4825764518@users.noreply.github.com>
2022-06-30 14:13:48 -0400

    xfce: add qalculate

--- apps/xfce.nix
+++ apps/xfce.nix
@@ -4,9 +4,11 @@
   environment.systemPackages = with pkgs; [
     (callPackage ../pkgs/xfce4-docklike-plugin {
       # 🤢
-      inherit (xfce) mkXfceDerivation libxfce4ui libxfce4util xfce4-panel xfconf;
+      inherit (xfce)
+        mkXfceDerivation libxfce4ui libxfce4util xfce4-panel xfconf;
     })
     xfce.xfce4-whiskermenu-plugin
+    qalculate-gtk
   ];
 
   xdg.portal.enable = true;
