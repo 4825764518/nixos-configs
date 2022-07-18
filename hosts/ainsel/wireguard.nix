2606bd0a75913bb6dbc2dedc3f2529d5b56f4e26
4825764518 <100122841+4825764518@users.noreply.github.com>
2022-07-17 20:06:32 -0400

    ainsel/leyndell: add wireguard config for mullvad socks5

--- hosts/ainsel/wireguard.nix
+++ hosts/ainsel/wireguard.nix
@@ -6,6 +6,9 @@ in {
     sops.secrets.ainsel-wireguard-privkey = {
       sopsFile = ../../secrets/ainsel/wireguard.yaml;
     };
+    sops.secrets.ainsel-wireguard-mullvad-privkey = {
+      sopsFile = ../../secrets/ainsel/wireguard.yaml;
+    };
     networking.wireguard.interfaces = {
       wg-internal = {
         ips = [
@@ -21,6 +24,16 @@ in {
           (wireguardPeers.serverPeers.mornePeer false)
           wireguardPeers.serverPeers.ovhPeer
         ];
+      };
+      wg-mullvad-fi1 = {
+        privateKeyFile =
+          "${config.sops.secrets.ainsel-wireguard-mullvad-privkey.path}";
+        ips = [ "10.66.75.124/32" ];
+        peers = [{
+          publicKey = "rGL76wTvmRKI2f8VHdGEZnTQbwRG4+RTO1sokGouVGU=";
+          allowedIPs = [ "10.64.0.1/32" ];
+          endpoint = "185.204.1.203:51820";
+        }];
       };
     };
   };
--- hosts/leyndell/wireguard.nix
+++ hosts/leyndell/wireguard.nix
@@ -6,6 +6,9 @@ in {
     sops.secrets.leyndell-wireguard-privkey = {
       sopsFile = ../../secrets/leyndell/wireguard.yaml;
     };
+    sops.secrets.leyndell-wireguard-mullvad-privkey = {
+      sopsFile = ../../secrets/leyndell/wireguard.yaml;
+    };
     networking.wireguard.interfaces = {
       wg-internal = {
         ips = [ "192.168.172.10/24" "fdc3:62d8:4c3a:0010::/64" ];
@@ -17,6 +20,16 @@ in {
           (wireguardPeers.serverPeers.mornePeer false)
           wireguardPeers.serverPeers.ovhPeer
         ];
+      };
+      wg-mullvad-fi1 = {
+        privateKeyFile =
+          "${config.sops.secrets.leyndell-wireguard-mullvad-privkey.path}";
+        ips = [ "10.65.222.173/32" ];
+        peers = [{
+          publicKey = "rGL76wTvmRKI2f8VHdGEZnTQbwRG4+RTO1sokGouVGU=";
+          allowedIPs = [ "10.64.0.1/32" ];
+          endpoint = "185.204.1.203:51820";
+        }];
       };
     };
   };
